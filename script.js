a8adec8ae5f66e21368cb258888cbedca291deaa
anvit285 <anvit688@gmail.com>
2025-02-14 18:44:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+ 
+    const ct = {};
+
+    // Count occurrences of each character
+    for (const ch of str) {
+        cct[ch] = (ct[ch] || 0) + 1;
+    }
+
+    // Find first non-repeating character
+    for (const ch of str) {
+        if (charCount[ch] === 1) {
+            return ch;
+        }
+    }
+
+    return "No non-repeating character found";
 }
+
+// Get user input
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
